358a9b3a8095c0720991c2a63c5aee884f9bb52a
Xianhui Zhu <xz2715@nyu.edu>
2021-05-27 17:24:47 -0400

    implement first action

--- src/App.js
+++ src/App.js
@@ -8,24 +8,45 @@ import cartItems from "./cart-items";
 
 // store - stores data, think of state
 // reducer - function that used to update store
+// action - what happened/ what update
+// return updated or old state
 import { createStore } from 'redux'
 
-function reducer(){
-  console.log("reducer ready to work")
+
+// dispatch method - send actions to the store
+// actions (objects) - MIST HAVE TYPE - what kind of action
+// DON'T MUTATE THE STATE - redux built on IMMUTABILITY (copy)
+
+// initial store
+const initialStore = {
+  count: 78
 }
 
-const store = createStore(reducer); 
+// reducer
+function reducer(state, action){
+  console.log({ state, action })
+  // decide which action to do based on the arg action
+  if(action.type === 'DECREASE'){
+    // return a copy instead of do state.count -= 1
+    return {count: state.count - 1}
+  }
+  return state
+}
+
+// store
+const store = createStore(reducer, initialStore)
+store.dispatch({type:'DECREASE'})
+console.log(store.getState())
 
 
 function App() {
   // cart setup
-
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()}/>
       <CartContainer cart={cartItems} />
     </main>
-  );
+  )
 }
 
 export default App;
