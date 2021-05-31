12fc51280e6ccecbfcbe5ac291032b9b1e4ce9c5
Xianhui Zhu <xz2715@nyu.edu>
2021-05-31 12:54:38 -0400

    make action type be variables instead of strings

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import cartItems from "./cart-items";
 // return updated or old state
 import { createStore } from 'redux'
 
+import { DECREASE, INCREASE, CLEAR } from './actions.js' 
 
 // dispatch method - send actions to the store
 // actions (objects) - MIST HAVE TYPE - what kind of action
@@ -19,32 +20,36 @@ import { createStore } from 'redux'
 
 // initial store
 const initialStore = {
-  count: 78
+  count: 0, 
+  name: "John"
 }
+
 
 // reducer
 function reducer(state, action){
   console.log({ state, action })
   // decide which action to do based on the arg action
-  if(action.type === 'DECREASE'){
+  if(action.type === DECREASE){
     // return a copy instead of do state.count -= 1
-    return { count: state.count - 1} 
+    // ...state copy all the state value
+    return { ...state, count: state.count - 1} 
   }
-  if(action.type === 'INCREASE'){
-    return { count: state.count + 1 }
+  if(action.type === INCREASE){
+    return { ...state, count: state.count + 1 }
   }
-  if(action.type === 'RESET'){
-    return { count: 0 }
+  if(action.type === CLEAR){
+    return { ...state, count: 0 }
   }
+  // if nothing happened return original state
   return state
 }
 
 // store
 const store = createStore(reducer, initialStore)
-store.dispatch({ type: 'DECREASE' }) // send actions
-store.dispatch({ type: 'INCREASE' })
-store.dispatch({ type: 'INCREASE'})
-store.dispatch({ type: 'RESET'})
+store.dispatch({ type: DECREASE }) // send actions
+store.dispatch({ type: INCREASE })
+store.dispatch({ type: INCREASE})
+store.dispatch({ type: CLEAR})
 console.log(store.getState())
 
 
