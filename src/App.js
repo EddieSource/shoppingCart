f2229b8944e66f8431b30cb8c07dd84e56185725
Xianhui Zhu <xz2715@nyu.edu>
2021-05-31 12:34:34 -0400

    add reducer and cooresponding dispatch methods

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
-// store - stores data, think of state
-// reducer - function that used to update store
+// store - stores data, think of state, need initial state
+// reducer - function that used to update store, think of setState
 // action - what happened/ what update
 // return updated or old state
 import { createStore } from 'redux'
@@ -28,19 +28,29 @@ function reducer(state, action){
   // decide which action to do based on the arg action
   if(action.type === 'DECREASE'){
     // return a copy instead of do state.count -= 1
-    return {count: state.count - 1}
+    return { count: state.count - 1} 
+  }
+  if(action.type === 'INCREASE'){
+    return { count: state.count + 1 }
+  }
+  if(action.type === 'RESET'){
+    return { count: 0 }
   }
   return state
 }
 
 // store
 const store = createStore(reducer, initialStore)
-store.dispatch({type:'DECREASE'})
+store.dispatch({ type: 'DECREASE' }) // send actions
+store.dispatch({ type: 'INCREASE' })
+store.dispatch({ type: 'INCREASE'})
+store.dispatch({ type: 'RESET'})
 console.log(store.getState())
 
 
 function App() {
   // cart setup
+  
   return (
     <main>
       <Navbar cart={store.getState()}/>
