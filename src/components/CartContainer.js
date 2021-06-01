510afbb759a9d03fe4d515fc9115d849036e85c1
Xianhui Zhu <xz2715@nyu.edu>
2021-05-31 21:40:00 -0400

    implement increase, decrease, and remove function in the reducer

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,8 +1,9 @@
 import React from "react";
 import CartItem from "./CartItem";
 import { connect } from "react-redux"
+import { CLEAR_CART } from "../actions.js"
 
-const CartContainer = ({ cart = [], total }) => {
+const CartContainer = ({ cart = [], total, dispatch }) => {
   if (cart.length === 0) {
     return (
       <section className="cart">
@@ -34,14 +35,19 @@ const CartContainer = ({ cart = [], total }) => {
             total <span>$0.00</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={()=>dispatch({type:CLEAR_CART})}>clear cart</button>
       </footer>
     </section>
   );
 };
 
+//map state from the store to the data input of the component
 const mapStateToProps = (store) => {
   return {cart: store.cart, total: store.total}
 }
 
+//The connect() function connects a React component to a Redux store. 
+// It provides its connected component with the pieces of the data it needs from the store, 
+// and the functions it can use to dispatch actions to the store. 
+// export the connected component
 export default connect(mapStateToProps)(CartContainer); 
--- src/reducer.js
+++ src/reducer.js
@@ -1,21 +1,65 @@
-import { DECREASE, INCREASE, CLEAR } from './actions.js' 
+import { bindActionCreators } from 'redux'
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from './actions' 
 
 // reducer
 function reducer(state, action){
     console.log({ state, action })
     // decide which action to do based on the arg action
-    if(action.type === DECREASE){
-      // return a copy instead of do state.count -= 1
-      // ...state copy all the state value
-      return { ...state, count: state.count - 1} 
+    let newCart = []
+    switch(action.type){
+        case CLEAR_CART: 
+            return { ...state, cart: []}
+        case DECREASE: 
+            if(action.payload.amount === 1){
+                // remove the specific item, use filter
+                return {
+                    ...state, 
+                    cart: state.cart.filter((cartItem)=>cartItem.id !== action.payload.id)
+                }
+            }
+            else {
+                newCart = state.cart.map((cartItem)=>{
+                    if(cartItem.id === action.payload.id){
+                        cartItem = {...cartItem, amount: cartItem.amount - 1}
+                    }
+                    return cartItem
+                })
+            }
+
+            return {...state, cart: newCart}
+        case INCREASE: 
+            // no need to remove, just update, so use map
+            newCart = state.cart.map((cartItem)=>{
+                if(cartItem.id === action.payload.id){
+                    cartItem = {...cartItem, amount: cartItem.amount + 1}
+                }
+                return cartItem
+            })
+            return {
+                ...state, 
+                cart: newCart}
+        case REMOVE: 
+            return {
+                ...state, 
+                cart: state.cart.filter((cartItem)=>cartItem.id !== action.payload.id)
+            }
+
+        default: // if nothing happened return original state
+            return state
+        
     }
-    if(action.type === INCREASE){
-      return { ...state, count: state.count + 1 }
-    }
-    if(action.type === CLEAR){
-      return { ...state, count: 0 }
-    }
-    // if nothing happened return original state
-    return state
+    // if(action.type === DECREASE){
+    //   // return a copy instead of do state.count -= 1
+    //   // ...state copy all the state value
+    //   return { ...state, count: state.count - 1} 
+    // }
+    // if(action.type === INCREASE){
+    //   return { ...state, count: state.count + 1 }
+    // }
+    // if(action.type === CLEAR_CART){
+    //   return { ...state, cart: [], count: 0 }
+    // }
+    // // if nothing happened return original state
+    // return state
 }
 export default reducer
